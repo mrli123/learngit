0925fa4427c606bebb96071a5c5d31921519be77
mrli123 <654876264@qq.com>
2016-08-18 23:07:36 +0800

    run

--- readme.c
+++ readme.c
@@ -1,8 +1,9 @@
-asdfasd
-asdfasd
-fsasdf
-as
-dfa
-sdf
-as
-dfasdfas
+#include <stdio.h>
+
+int main(void)
+{
+	int i ;
+	printf("\t this is a test fot git-hub\n");
+	printf("\t My name is mrli123\n");
+	return 0;
+}
